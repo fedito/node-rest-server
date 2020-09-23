85a27da2cab7825b99836b4ddfca479786ce19ec
Fede <federico.scag@gmail.com>
2020-09-22 21:21:54 -0300

    a

--- server/config/config.js
+++ server/config/config.js
@@ -11,7 +11,7 @@ process.env.PORT = process.env.PORT || 3000;
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 //==================================
-//         PUERTO
+//        ??????
 //==================================
 
 let urlDB;
